20e2b9739338c7b2ca0e5f0183c2d0e06f8b9dc6
srive <sri.venkatraghavan@gmail.com>
2021-01-10 14:18:19 +0530

    Learned @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)

--- src/main/java/me/learning/jpa/entity/Employee.java
+++ src/main/java/me/learning/jpa/entity/Employee.java
@@ -1,6 +1,5 @@
 package me.learning.jpa.entity;
 
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,8 +7,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "EmployeeType")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Employee {
 
 	@Id
